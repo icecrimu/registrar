5cf3b1a133823ca44103aa6f9b544cfb5ee1ed30
icecrimu <charles.banigon@gmail.com>
2022-06-15 05:56:20 +0800

    Add favicon

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        
+        <link rel="icon" type="image/x-icon" href="<?php echo URLROOT;?>/public/img/logo.png">
         <link rel="stylesheet"  href="<?php echo URLROOT;?>/public/bootstrap-5.1.3-dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="<?php echo URLROOT;?>/public/css/assets.css">
         <link rel="stylesheet" href="<?php echo URLROOT;?>/public/css/style.css">
