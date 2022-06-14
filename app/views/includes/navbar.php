d768c07b5b7bad45da8341aec5c6dd32105a1328
icecrimu <charles.banigon@gmail.com>
2022-06-15 05:52:32 +0800

    Change Brand Logo

--- app/views/includes/navbar.php
+++ app/views/includes/navbar.php
@@ -1,13 +1,12 @@
 <div>
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-2 mb-4 border-bottom bg-light">
-        <a href="<?php echo URLROOT . '/users/index' ?>" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-decoration-none ms-2">
-            <img src="<?php echo URLROOT .'/img/logo.png' ?> " class="img-fluid" width="50" height="50">
+        <a href="<?php echo URLROOT . '/users/' ?>" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-decoration-none ms-2 ">
+            <img src="<?php echo URLROOT .'/img/psu.png' ?> " class="d-inline-block align-text-top"  height="50">
         </a>
 
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">Logs</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">Settings</a></li>
+            <li><a href="<?php echo URLROOT; ?>/" class="nav-link px-2 link-secondary">Home</a></li>
+            <li><a href="<?php echo URLROOT; ?>/users/settings" class="nav-link px-2 link-dark">Settings</a></li>
         </ul>
 
         <div class="col-md-3 text-end">
